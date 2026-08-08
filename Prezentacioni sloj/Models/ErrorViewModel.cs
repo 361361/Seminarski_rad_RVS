8745579d6f1e71bb03d645817d24ab258dd7aa98
361361 <davidgluvakov@gmail.com>
2026-08-08 18:36:05 +0200

    Prezentacioni sloj osnova

---
+++ Prezentacioni sloj/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prezentacioni_sloj.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
