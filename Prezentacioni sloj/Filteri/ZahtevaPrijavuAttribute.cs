a18ad1a4dae1bef562c7017cd0d5e603cb06094f
361361 <davidgluvakov@gmail.com>
2026-08-08 19:59:59 +0200

    Filteri

---
+++ Prezentacioni sloj/Filteri/ZahtevaPrijavuAttribute.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace RVS_Aplikacija.Filteri
+{
+    // Action filter - primenjuje se na kontrolere/akcije kojima je potrebna prijava.
+    // Ako u sesiji nema "KorisnikId", korisnik se preusmerava na formu za prijavu.
+    public class ZahtevaPrijavuAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var sesija = context.HttpContext.Session;
+            if (sesija.GetInt32("KorisnikId") == null)
+            {
+                context.Result = new RedirectToActionResult("Prijava", "Nalog", null);
+                return;
+            }
+            base.OnActionExecuting(context);
+        }
+    }
+}
